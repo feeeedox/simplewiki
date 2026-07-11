2b863c1774efa0a6c53c8281d3f0902bed8090b9
Florian Ohldag <mrminecrafttv19@gmail.com>
2026-07-11 16:30:49 +0200

    actually idk anymore

--- app/components/content/Keyboard.vue
+++ app/components/content/Keyboard.vue
@@ -8,4 +8,4 @@ defineProps<{
   <span class="inline-flex items-center gap-0.5">
     <kbd v-for="(key, i) in keys.split('+')" :key="i" class="bg-header-band border border-border border-b-2 rounded-xs py-px px-1.25 text-text text-[0.7188rem] font-mono">{{ key }}</kbd>
   </span>
-</template>
+</template> 

4b9a73ad7925ea70fc4d3dfce95bbd3fea2e0e2e
Florian Ohldag <mrminecrafttv19@gmail.com>
2026-07-11 16:17:08 +0200

    idk

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,32 +6,39 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   css: ["@/assets/css/main.css"],
-  modules: ["@nuxt/content", "@nuxt/fonts", "@nuxt/image", "@nuxt/scripts", "nuxt-og-image"],
+  modules: [
+    "@nuxt/content",
+    "@nuxt/fonts",
+    "@nuxt/image",
+    "@nuxt/scripts",
+    "nuxt-og-image",
+  ],
   nitro: {
     preset: "github-pages",
     prerender: {
       routes: ["/"],
-      crawlLinks: true
+      crawlLinks: true,
+      autoSubfolderIndex: true,
     },
   },
-  ogImage: {
-    zeroRuntime: true,
+  experimental: {
+    asyncContext: true,
   },
   app: {
-    baseURL: '/simplewiki/', // Change this to your desired base URL, or remove it if you want to serve from the root.
-    cdnURL: '/simplewiki/', // Change this to your desired CDN URL, or remove it if you want to serve from the root.
+    baseURL: "/simplewiki/", // Change this to your desired base URL, or remove it if you want to serve from the root.
+    cdnURL: "/simplewiki/", // Change this to your desired CDN URL, or remove it if you want to serve from the root.
     head: {
-      base: { href: '/simplewiki/' }, // Change this to your desired base URL, or remove it if you want to serve from the root.
+      base: { href: "/simplewiki/" }, // Change this to your desired base URL, or remove it if you want to serve from the root.
       link: [
-        { rel: 'icon', href: 'favicon.ico', sizes: '32x32' },
-        
-        { rel: 'icon', href: 'icon.svg', type: 'image/svg+xml' },
-        
-        { rel: 'apple-touch-icon', href: 'apple-touch-icon.png' },
-        
-        { rel: 'manifest', href: 'site.webmanifest' }
-      ]
-    }
+        { rel: "icon", href: "favicon.ico", sizes: "32x32" },
+
+        { rel: "icon", href: "icon.svg", type: "image/svg+xml" },
+
+        { rel: "apple-touch-icon", href: "apple-touch-icon.png" },
+
+        { rel: "manifest", href: "site.webmanifest" },
+      ],
+    },
   },
   content: {
     build: {
@@ -42,7 +49,10 @@ export default defineNuxtConfig({
         highlight: {
           theme: "github-light",
           langs: Object.keys(bundledLanguages).filter(
-            (lang) => !lang.includes("+") && !lang.includes("#") && !lang.includes("文")
+            (lang) =>
+              !lang.includes("+") &&
+              !lang.includes("#") &&
+              !lang.includes("文"),
           ) as BundledLanguage[],
         },
       },
